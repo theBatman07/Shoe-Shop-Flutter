24b0a99c3af5dd0c1aff99a9d11584527802e1a7
G.Khater <g.khater07@gmail.com>
2023-12-01 04:18:31 +0530

    first commit

---
+++ lib/global_variables.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Kike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Kike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '1',
+    'title': 'Aliodas Shoes',
+    'price': 20.12,
+    'imageUrl': 'assets/images/shoes_2.png',
+    'company': 'Aliodas',
+    'sizes': [9, 10, 12],
+  },
+  {
+    'id': '2',
+    'title': 'VimBruh Women\'s Shoes',
+    'price': 28.95,
+    'imageUrl': 'assets/images/shoes_3.png',
+    'company': 'VimBruh',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '3',
+    'title': 'Cloudy Shoes',
+    'price': 420.69,
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company': 'Kike',
+    'sizes': [8, 9, 10],
+  },
+];
